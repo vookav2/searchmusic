cb600c774242a52c1baa49960c9cef82fae8e126
mr687 <davinomoehdanino@gmail.com>
2022-06-05 13:54:35 +0700

    add: hash property

--- src/entities/album.ts
+++ src/entities/album.ts
@@ -1,29 +1,32 @@
+import { hashMd5, safety } from '../utils'
+
 import { Channel } from './channel'
 import { Playlist } from './playlist'
-import { safety } from '../utils'
 import { ytError } from '../yt-scraper'
 
 export type Album = {
-	id: string
-	title: string
-	thumbnail: string
-	explicit: boolean
-	channel?: Pick<Channel, 'id' | 'name'>
-	getPlaylist: () => Promise<Playlist>
+  id: string
+  title: string
+  thumbnail: string
+  explicit: boolean
+  hash: string
+  channel?: Pick<Channel, 'id' | 'name'>
+  getPlaylist: () => Promise<Playlist>
 }
 
 export const makeAlbum = ({
-	id,
-	thumbnail,
-	explicit,
-	title,
-	channel,
-	getPlaylist,
-}: Album): Album => ({
-	id: safety(id).required(ytError.noContent),
-	title: safety(title).required(ytError.noContent),
-	channel: safety(channel).required(ytError.noContent),
-	thumbnail: safety(thumbnail).required(ytError.noContent),
-	explicit: explicit ?? false,
-	getPlaylist: safety(getPlaylist).undefined(ytError.noContent),
+  id,
+  thumbnail,
+  explicit,
+  title,
+  channel,
+  getPlaylist,
+}: Omit<Album, 'hash'>): Album => ({
+  id: safety(id).required(ytError.noContent),
+  title: safety(title).required(ytError.noContent),
+  channel: safety(channel).required(ytError.noContent),
+  thumbnail: safety(thumbnail).required(ytError.noContent),
+  explicit: explicit ?? false,
+  hash: hashMd5(`${id}${title}${channel?.name}`),
+  getPlaylist: safety(getPlaylist).undefined(ytError.noContent),
 })
