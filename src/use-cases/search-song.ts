48a3edb33e0cd22f56fabdb0fb6d859fe8a23770
mr687 <dapino.dev@gmail.com>
2023-04-02 00:05:01 +0700

    fix search function when the result type is song

--- src/use-cases/search-song.ts
+++ src/use-cases/search-song.ts
@@ -1,61 +1,69 @@
 import {
-	joinRunsText,
-	makePlaylistFunc,
-	mapBrowseId,
-	mapThumbnail,
-	ytSearch,
+  joinRunsText,
+  makePlaylistFunc,
+  mapBrowseId,
+  mapNavigationEndpoint,
+  mapThumbnail,
+  ytSearch,
 } from '../yt-scraper'
 
 import { makeSong } from '../entities'
 
 export const songMapping = (raw: any) => {
-	const [rawTitle, secondColumn] = raw.flexColumns
-	const rawDetail =
-		secondColumn.musicResponsiveListItemFlexColumnRenderer.text.runs
-	if (rawDetail.length > 5) {
-		rawDetail.splice(0, 2)
-	}
-	const [rawArtist, , rawAlbum, , rawDuration] = rawDetail
-	const { videoId, playlistId, params } =
-		raw.overlay.musicItemThumbnailOverlayRenderer.content
-			.musicPlayButtonRenderer.playNavigationEndpoint.watchEndpoint
-	const albumBrowseId = mapBrowseId(rawAlbum)
-	return makeSong({
-		id: videoId,
-		title: joinRunsText(
-			rawTitle.musicResponsiveListItemFlexColumnRenderer.text
-		),
-		album: albumBrowseId
-			? {
-					id: albumBrowseId,
-					title: rawAlbum.text,
-			  }
-			: undefined,
-		channel: {
-			id: mapBrowseId(rawArtist),
-			name: rawArtist.text,
-		},
-		explicit: raw.badges !== undefined,
-		selected: false,
-		thumbnail: mapThumbnail(raw.thumbnail),
-		durationString: rawDuration.text,
-		getPlaylist: makePlaylistFunc({
-			videoId,
-			playlistId,
-			params,
-		}),
-	})
+  const [rawTitle, secondColumn] = raw.flexColumns
+    ? raw.flexColumns
+    : [raw.title, raw.subtitle.runs]
+  const rawDetail =
+    secondColumn.musicResponsiveListItemFlexColumnRenderer?.text.runs ||
+    secondColumn
+  if (rawDetail.length > 5) {
+    rawDetail.splice(0, 2)
+  }
+  const [rawArtist, , rawAlbum, , rawDuration] = rawDetail
+  const { videoId, playlistId, params } =
+    raw.overlay?.musicItemThumbnailOverlayRenderer?.content
+      .musicPlayButtonRenderer?.playNavigationEndpoint.watchEndpoint ||
+    mapNavigationEndpoint(
+      raw.menu.menuRenderer.items.at(0)?.menuNavigationItemRenderer
+        .navigationEndpoint,
+    )
+  const albumBrowseId = mapBrowseId(rawAlbum)
+  return makeSong({
+    id: videoId,
+    title: joinRunsText(
+      rawTitle.musicResponsiveListItemFlexColumnRenderer?.text || rawTitle,
+    ),
+    album: albumBrowseId
+      ? {
+          id: albumBrowseId,
+          title: rawAlbum.text,
+        }
+      : undefined,
+    channel: {
+      id: mapBrowseId(rawArtist),
+      name: rawArtist.text,
+    },
+    explicit: raw.badges !== undefined,
+    selected: false,
+    thumbnail: mapThumbnail(raw.thumbnail),
+    durationString: rawDuration.text,
+    getPlaylist: makePlaylistFunc({
+      videoId,
+      playlistId,
+      params,
+    }),
+  })
 }
 
 export const searchSong = async (query: string) => {
-	const result = await ytSearch(query, 'Songs')
-	const song = songMapping(result.rawContent)
-	return {
-		search: {
-			type: result.rawType,
-			query: result.query,
-			correctedQuery: result.correctedQuery,
-		},
-		song,
-	}
+  const result = await ytSearch(query, 'Songs')
+  const song = songMapping(result.rawContent)
+  return {
+    search: {
+      type: result.rawType,
+      query: result.query,
+      correctedQuery: result.correctedQuery,
+    },
+    song,
+  }
 }
