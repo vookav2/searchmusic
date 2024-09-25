a137fa1fec89154396aaa929065759572eddef10
Davi Nomoeh Dani <26916086+mr687@users.noreply.github.com>
2024-09-25 19:25:52 +0700

    bump youtube version and fix playlist title (#146)

--- src/yt-scraper/yt-request.ts
+++ src/yt-scraper/yt-request.ts
@@ -1,38 +1,38 @@
 import got, { Headers, Options } from 'got'
 
 const httpHeaders: Headers = {
-	origin: 'https://music.youtube.com',
-	'User-Agent':
-		'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-	'Accept-Language': 'en',
-	referer: 'https://music.youtube.com/',
+  origin: 'https://music.youtube.com',
+  'User-Agent':
+    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+  'Accept-Language': 'en',
+  referer: 'https://music.youtube.com/',
 }
 const searchParams = new URLSearchParams([
-	['alt', 'json'],
-	['key', 'AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30'],
+  ['alt', 'json'],
+  ['key', 'AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30'],
 ])
 const httpOptions: Options = {
-	method: 'POST',
-	prefixUrl: 'https://music.youtube.com/youtubei/v1/',
-	headers: httpHeaders,
-	responseType: 'json',
-	resolveBodyOnly: true,
-	searchParams,
+  method: 'POST',
+  prefixUrl: 'https://music.youtube.com/youtubei/v1/',
+  headers: httpHeaders,
+  responseType: 'json',
+  resolveBodyOnly: true,
+  searchParams,
 }
 const bodyContext = {
-	capabilities: {},
-	client: {
-		clientName: 'WEB_REMIX',
-		clientVersion: '0.1',
-		hl: 'en',
-		gl: 'US',
-	},
+  capabilities: {},
+  client: {
+    clientName: 'WEB_REMIX',
+    clientVersion: '1.20240918.01.00',
+    hl: 'en',
+    gl: 'US',
+  },
 }
 
 const httpClient = got.extend(httpOptions)
 export const ytRequest = (path: string, body: Record<string, any>) => {
-	Object.assign(body, { context: bodyContext })
-	return httpClient<unknown>(path, {
-		json: body,
-	})
+  Object.assign(body, { context: bodyContext })
+  return httpClient<unknown>(path, {
+    json: body,
+  })
 }
